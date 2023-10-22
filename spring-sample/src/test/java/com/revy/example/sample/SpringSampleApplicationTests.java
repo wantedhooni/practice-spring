ba7448aad229a13fca122aa12ce37e24d74e76c5
Revy <wantedhooni@gmail.com>
2023-10-23 01:56:32 +0900

    INIT: sample 프로젝트 추가

---
+++ spring-sample/src/test/java/com/revy/example/sample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revy.example.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
