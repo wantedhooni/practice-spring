1811bd88a8b34e03f784d2c031f23b106d099eb0
Revy <wantedhooni@gmail.com>
2023-10-22 18:19:48 +0900

    INIT: 프로젝트 셋팅

---
+++ spring-master-slave/src/main/java/com/revy/example/db_master_slave/SpringMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.revy.example.db_master_slave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMasterSlaveApplication.class, args);
+    }
+
+}
---
+++ spring-master-slave/src/test/java/com/revy/example/db_master_slave/SpringMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revy.example.db_master_slave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
