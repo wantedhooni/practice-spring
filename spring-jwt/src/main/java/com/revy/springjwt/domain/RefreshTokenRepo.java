7d183f9cfd4736b0c7ddac3e3d333a920ed4d6d2
Revy <wantedhooni@gmail.com>
2023-10-24 20:57:43 +0900

    FEAT: repo 쿼리 추가

--- spring-jwt/src/main/java/com/revy/springjwt/domain/RefreshTokenRepo.java
+++ spring-jwt/src/main/java/com/revy/springjwt/domain/RefreshTokenRepo.java
@@ -2,5 +2,9 @@ package com.revy.springjwt.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepo extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
 }
--- spring-jwt/src/main/java/com/revy/springjwt/domain/UserRepo.java
+++ spring-jwt/src/main/java/com/revy/springjwt/domain/UserRepo.java
@@ -2,5 +2,9 @@ package com.revy.springjwt.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
