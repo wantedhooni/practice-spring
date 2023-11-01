5aff59e24b5127f22abe3109ef3345a1068c7dc0
Revy <wantedhooni@gmail.com>
2023-11-01 14:14:46 +0900

    FEAT: Example Endpoint 작성

--- spring3-Swagger/src/main/java/com/revy/spring3swagger/endpoint/ExampleController.java
+++ spring3-Swagger/src/main/java/com/revy/spring3swagger/endpoint/ExampleController.java
@@ -1,8 +1,30 @@
 package com.revy.spring3swagger.endpoint;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/example")
 public class ExampleController {
+
+
+    @GetMapping
+    public String get() {
+        return "get";
+    }
+
+    @PostMapping
+    public String post() {
+        return "post";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "delete";
+    }
+
+    @PutMapping
+    public String put() {
+        return "put";
+    }
 
 }
